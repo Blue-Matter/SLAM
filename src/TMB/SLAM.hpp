76a503953c0405dd2d7601765a506ae3d40ee5ec
Adrian <ar.hordyk@gmail.com>
2022-05-17 12:32:18 -0700

    Update SLAM.hpp

--- src/TMB/SLAM.hpp
+++ src/TMB/SLAM.hpp
@@ -328,7 +328,7 @@ Type SLAM(objective_function<Type>* obj) {
 
   // penalty random walk in F after initial
   for (int m=1; m<n_months; m++) {
-    nll_joint(4) -= dnorm(F_m(m), F_m(m-1), sigmaF, true);
+    nll_joint(4) -= dnorm(F_m(m), F_m(m-1), Type(0.2), true);
   }
 
   // penalty random walk in monthly R0
