4482497faf735d0fcdac6432723b51f4b988ed72
Adrian <ar.hordyk@gmail.com>
2022-06-07 14:24:24 -0700

    Update optF.hpp

--- src/TMB/optF.hpp
+++ src/TMB/optF.hpp
@@ -109,6 +109,7 @@ Type optF(objective_function<Type>* obj) {
       }
       SB_a(a) = N_m(a,m_ind) * Wght_Age(a) * Mat_at_Age(a);
     }
+    SB(m_ind) = SB_a.sum();
     Rec(m_ind) = BHH_SRR(rec_pattern(m_ind), h, SB(m_ind), SBpR);
     N_m(0,m_ind) = Rec(m_ind);
   }
