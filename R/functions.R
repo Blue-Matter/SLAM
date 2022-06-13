50918486043f310b447900f22329dbc2a4c41ae8
Adrian <ar.hordyk@gmail.com>
2022-06-13 11:13:33 -0700

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -40,7 +40,12 @@ Load_Scenario <- function(scen=1) {
   vals <- Scenario_Parameters[,scen+1]
   List <- list()
   for (i in 1:length(vars)) {
-    List[[vars[i]]] <- as.numeric(vals[i])
+    if (vars[i]!='effortPattern') {
+      List[[vars[i]]] <- as.numeric(vals[i])
+    } else {
+      List[[vars[i]]] <- vals[i]
+    }
+
   }
   List
 }
@@ -63,4 +68,57 @@ BH_SRR <- function(R0, h, SB, SBpR) {
 logit <- function(p) log(p/(1-p))
 inv.logit <- function(x) exp(x)/(1+exp(x))
 
+
+SimData <- function(Pars, CAL_ESS=200, Effort_SD=0.2, CPUE_SD=0.2,
+                    Fmeanprior=c(0.2, 0.4),
+                    fitCPUE=1, fitEffort=1,
+                    use_Frwpen=1,
+                    use_R0rwpen=1,
+                    use_years=5) {
+  # simulate fishery
+  Pop <- Simulate(Pars)
+
+  # Populate data
+  sim.timesteps <- Pars$nyears*12
+  data.timesteps <- (sim.timesteps-use_years*12+1):sim.timesteps
+  nts <- length(data.timesteps)
+  nages <- length(Pop$Len_at_Age)
+
+  Data <- list()
+  Data$Len_Age <- Pop$Len_at_Age
+  Data$SD_Len_at_Age <- Pop$SD_Len_at_Age
+  Data$Wt_at_Age <- Pop$Wt_at_Age
+  Data$Mat_at_Age <- Pop$Mat_at_Age
+  Data$M_at_Age <- Pop$M_at_Age
+  Data$phi_at_Age <- Pop$phi_at_Age
+  Data$Len_Bins <- Pop$Len_Bins
+  Data$Len_Mids <- Pop$Len_Mids
+  Data$CAL <- Pop$CAL_samp[,data.timesteps]
+  Data$CAL_ESS <- rep(CAL_ESS, nts)
+  Data$Effort <- Pop$Effort_ind[data.timesteps]/mean(Pop$Effort_ind[data.timesteps],na.rm=TRUE)
+  Data$Effort_SD <- rep(Effort_SD, nts)
+  Data$CPUE <- Pop$Index[data.timesteps]/mean(Pop$Index[data.timesteps])
+  Data$CPUE_SD <- rep(CPUE_SD, nts)
+
+  if (!is.null(Fmeanprior)) {
+    Data$use_Fmeanprior <- 1
+    Data$F_meanprior <- Fmeanprior
+  } else {
+    Data$use_Fmeanprior <- 0
+    Data$F_meanprior <- c(0.1,0.1)
+  }
+
+  Data$Fit_Effort <- fitEffort
+  Data$Fit_CPUE <- fitCPUE
+
+  Data$use_Frwpen <- use_Frwpen
+  Data$use_R0rwpen <- use_R0rwpen
+  Data$Fit_Effort <- fitEffort
+  Data$Fit_CPUE <- fitCPUE
+
+  out <- list()
+  out$Pop <- Pop
+  out$Data <- Data
+  out
+}
 
