eacb22f65bf1339b27a60727e2433d2e61e4c932
Adrian <ar.hordyk@gmail.com>
2022-06-28 14:31:47 -0700

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -31,16 +31,31 @@ every_nth = function(n) {
 
 
 # Generate monthly recruitment pattern
-GenMonthlyRec <- function(mu=6, sigma=2) {
-  tt <- dnorm(-5:6, 0, sigma)
-  tt <- tt/sum(tt)
-  t2 <- rep(tt, 5)
-  rec_pattern <- rep(0, 12)
-  n <- 12-mu
-  rec_pattern[mu:(mu+n)] <- t2[18:(18+n)]
-  if (mu>1)
-    rec_pattern[1:(mu-1)] <- t2[(18-mu+1):17]
-  rec_pattern
+GenMonthlyRec <- function(mu=4, sigma=2) {
+  n <- length(mu)
+  rec <- list()
+  for (i in 1:n) {
+    tt <- dnorm(0:11, 5.5, sigma[i])
+    tt <- tt/sum(tt)
+    tt <- rep(tt,5)
+    rec_pattern <- rep(0, 12)
+    rec_pattern[floor(mu[i])] <- tt[6]
+    j <- floor(mu[i])+1
+    k <- 1
+
+    while(j<=12) {
+      rec_pattern[j] <- tt[6+k]
+      j <- j+1
+      k <- k+1
+    }
+    for (j in 1:(floor(mu[i])-1)) {
+      rec_pattern[j] <- tt[6+k]
+      k <- k+1
+    }
+    rec[[i]] <- rec_pattern
+  }
+  rec <- do.call('rbind', rec)
+  apply(rec, 2, sum)/sum(apply(rec, 2, sum))
 }
 
 # Calculate selectivity at length
