135bbabbe05ff24bfa47ea696d37b79d01bcaa15
Adrian <ar.hordyk@gmail.com>
2022-06-28 14:31:26 -0700

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
-Scenario_Parameters <- readxl::read_excel('data-raw/Scenario_Parameters.xlsx')
+# Scenario_Parameters <- readxl::read_excel('data-raw/Scenario_Parameters.xlsx')
 
 casestudydata <- readRDS('data-raw/casestudydata.rds')
-usethis::use_data(Scenario_Parameters, casestudydata, overwrite = TRUE)
+usethis::use_data(casestudydata, overwrite = TRUE)
+# usethis::use_data(Scenario_Parameters, casestudydata, overwrite = TRUE)
 
