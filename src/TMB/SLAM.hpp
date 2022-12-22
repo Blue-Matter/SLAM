c269a9eacbdef1448dbcace98620a252fd4abed4
Adrian <ar.hordyk@gmail.com>
2022-12-22 13:17:42 -0800

    Update SLAM.hpp

--- src/TMB/SLAM.hpp
+++ src/TMB/SLAM.hpp
@@ -7,6 +7,7 @@
 template<class Type>
 Type SLAM(objective_function<Type>* obj) {
 
+  // ---- Data ----
   // At-Age Schedules
   DATA_VECTOR(Weight_Age);  // mean weight at age
   DATA_VECTOR(Weight_Age_SD);  // standard deviation of weight at age (log-normal)
@@ -40,91 +41,83 @@ Type SLAM(objective_function<Type>* obj) {
   DATA_INTEGER(use_R0rwpen);
   DATA_INTEGER(use_Fmeanprior);
 
-  // Estimated Parameters
+  // ---- Estimated Parameters ----
   PARAMETER(ls50);  // log age-at-50% selectivity
   PARAMETER(lsdelta); // log interval age-50 - age-95% selectivity
 
-  PARAMETER_VECTOR(logR0_m_est); // monthly R0 - fraction
-  PARAMETER(log_sigmaR0); // sd for random walk penalty in monthly R0
-
+  PARAMETER_VECTOR(logF_minit); // equilibrium fishing mortality for first age-classes
   PARAMETER_VECTOR(logF_m); // monthly fishing mortality
-  PARAMETER_VECTOR(logF_minit); // fishing mortality for first age-classes
+  PARAMETER(log_sigmaF); // standard deviation for random walk penalty for F
+
+  PARAMETER_VECTOR(logR0_m_est); // average fraction of annual recruitment in each month
+  PARAMETER(log_sigmaR0); // sd for random walk penalty for monthly recruitment
 
   PARAMETER_VECTOR(logRec_Devs); // monthly recruitment deviations
+  PARAMETER(log_sigmaR); // monthly rec dev sd (usually fixed)
 
-  //  variances
-  PARAMETER(log_sigmaF); // standard deviation for random walk penalty for F
-  PARAMETER(log_sigmaR); // monthly rec dev sd
-
-  // Index variables
-  int n_ages = Weight_Age.size();
-  int n_bins = WghtMids.size();
-  int n_months = CPUE.size();
-
-  // Seasonal Recruitment
-  vector<Type> logR0_m(12); // R0 for each calendar month
-  logR0_m.setZero();
-  for(int m=1;m<12;m++){
-    logR0_m(m) = logR0_m_est(m-1); // map monthly mean rec
-  }
-  vector<Type> R0_m(12);
-  R0_m.setZero();
-  R0_m = exp(logR0_m);
-  Type R0_mtotal = R0_m.sum();
-  // standardize to sum to 1
-  for(int m=0;m<12;m++){
-    R0_m(m) = R0_m(m)/R0_mtotal;
-  }
-
-  Type sigmaR0 = exp(log_sigmaR0); // SD for random walk in R0_m
+  // ---- Transform Parameters ----
   Type sigmaR = exp(log_sigmaR); // rec process error dev sd
+  Type sigmaR0 = exp(log_sigmaR0); // SD for random walk in R0_m
+  Type sigmaF = exp(log_sigmaF); // standard deviation for random walk penalty for F
 
-  // Fishing mortality
-  vector<Type> F_m(n_months);
-  F_m.setZero();
-  F_m = exp(logF_m); // monthly fishing mortality
-  Type sigmaF = exp(log_sigmaF); // standard deviation for inter-monthly variation in F
-
-  vector<Type> F_minit(n_ages); // fishing mortality for initial age classes
-  F_minit.setZero();
-  F_minit = exp(logF_minit);
-
-  // Transform selectivity parameters
   // Selectivity-at-Weight
   Type S50 = exp(ls50);
   Type Sdelta = exp(lsdelta);
   Type S95 = S50 + Sdelta;
 
-  // vector<Type> selW(n_bins);
-  // selW.setZero();
-  // for(int w=0;w<n_bins;w++){
-  //   selW(w) = 1 / (1 + exp(-log(Type(19))*(WghtMids(w) - S50)/Sdelta));
-  // }
+  // ---- indexing variables ----
+  int n_ages = Weight_Age.size(); // number of age classes
+  int n_bins = WghtMids.size(); // number of size bins
+  int n_months = CPUE.size(); // number of months of data
 
-  // Generate Age-Weight Key
+  // ---- Generate Age-Weight Key ----
   matrix<Type> AWK(n_ages, n_bins);
   AWK.setZero();
   AWK = generate_AWK(WghtBins, Weight_Age, Weight_Age_SD, n_ages, n_bins);
 
-  // Selectivity-at-Age
+  // ---- Seasonal Recruitment ----
+  vector<Type> logR0_m(12); // R0 for each calendar month
+  logR0_m.setZero();
+
+  // map monthly mean rec fixing first month at log(1)
+  for(int m=1;m<12;m++){
+    logR0_m(m) = logR0_m_est(m-1);
+  }
+
+  vector<Type> R0_m(12);
+  R0_m.setZero();
+  R0_m = exp(logR0_m); // fraction of recruitment in each calendar month
+  Type R0_mtotal = R0_m.sum();
+
+  // standardize to sum to 1
+  for(int m=0;m<12;m++){
+    R0_m(m) = R0_m(m)/R0_mtotal;
+  }
+
+  // ---- Fishing mortality ----
+  // fishing mortality for initial age classes
+  vector<Type> F_minit(n_ages);
+  F_minit.setZero();
+  F_minit = exp(logF_minit);
+
+  // monthly fishing mortality
+  vector<Type> F_m(n_months);
+  F_m.setZero();
+  F_m = exp(logF_m); // monthly fishing mortality
+
+  // ---- Selectivity-at-Age ----
   vector<Type> selA(n_ages);
   selA.setZero();
   for(int a=0;a<n_ages;a++){
     selA(a) = 1 / (1 + exp(-log(Type(19))*((a - S50)/Sdelta)));
   }
-  // for(int a=0;a<n_ages;a++){
-  //   vector<Type> temp(n_bins);
-  //   temp.setZero();
-  //   temp = AWK.row(a);
-  //   for(int w=0;w<n_bins;w++){
-  //     selA(a) += temp(w)*selW(w);
-  //   }
-  // }
 
-  // F, M, and Z by month and age
-  matrix<Type> F_ma(n_ages, n_months);
+
+  // --- Set up mortality matrices ----
   matrix<Type> M_ma(n_ages, n_months);
+  matrix<Type> F_ma(n_ages, n_months);
   matrix<Type> Z_ma(n_ages, n_months);
+
   F_ma.setZero();
   M_ma.setZero();
   Z_ma.setZero();
@@ -137,116 +130,52 @@ Type SLAM(objective_function<Type>* obj) {
     }
   }
 
-  // Calculate SPR
-  // Calculate spawning biomass per recruit
-  vector<Type> surv0(n_ages);
-  surv0.setZero();
-  surv0(0) = 1;
-  vector<Type> egg0(n_ages);
-  egg0.setZero();
-  for (int a=1; a<n_ages; a++) {
-    surv0(a) = surv0(a-1)*exp(-M_ma(a-1,0))*(1-PSM_at_Age(a-1));
-  }
-  for (int a=0; a<n_ages; a++) {
-    egg0(a) = surv0(a) * Weight_Age(a) * Mat_at_Age(a);
-  }
-  Type SBpR = egg0.sum();
 
-
-  matrix<Type> survF(n_ages, n_months);
-  survF.setZero();
-  vector<Type> eggFa(n_ages);
-  eggFa.setZero();
-  vector<Type> eggF(n_months);
-  eggF.setZero();
-
-  for (int m=0; m<n_months; m++) {
-    for(int a=0;a<n_ages;a++){
-      if (a==0) {
-        survF(a,m) = 1;
-      } else {
-        survF(a,m) = survF(a-1,m)*exp(-Z_ma(a-1, m)) * (1-PSM_at_Age(a-1));
-      }
-      eggFa(a) = survF(a,m) * Weight_Age(a) * Mat_at_Age(a);
-    }
-    eggF(m) = eggFa.sum();
-  }
-
-  vector<Type> SPR(n_months);
-  SPR.setZero();
-  for (int m=0; m<n_months; m++) {
-    SPR(m) = eggF(m)/SBpR;
-  }
-
-  // Population Dynamics - monthly time-step
-  // first month
+  // ---- Initialize unfished population ----
+  // run-out for 3 years to get rid of initial conditions
+  // and get equilibrium conditions for seasonal recruitment
   matrix<Type> N_unfished(n_ages, 12);
   N_unfished.setZero();
+  for (int t=0; t<36; t++) {
+    int m_ind = t % 12; // month index
+    for(int a=0;a<n_ages;a++){
+      if (a==0) {
+        N_unfished(a,m_ind) = R0_m(m_ind);
+      } else {
+        if (m_ind==0) {
+          N_unfished(a,m_ind) = N_unfished(a-1,11) * exp(-M_ma(a-1)) * (1-PSM_at_Age(a-1));
+        } else {
+          N_unfished(a,m_ind) = N_unfished(a-1,m_ind-1) * exp(-M_ma(a-1)) * (1-PSM_at_Age(a-1));
+        }
+      }
+    }
+  }
+
+  // ---- Biomass matrices and vector ----
+  matrix<Type> SB_am(n_ages, n_months); // SB by age and month
+  SB_am.setZero();
+  vector<Type> SB_m(n_months); // SB by month
+  SB_m.setZero();
+
+  // ---- Equilibrium initial fished population ----
   vector<Type> Z_init(n_ages);
-  Z_init.setZero();
-
-  vector<Type> SB_init(12);
-  SB_init.setZero();
-  matrix<Type> SB_ainit(n_ages, 12);
-  SB_ainit.setZero();
-
+  Z_init.setZero(); // total mortality for initial age classes
   for(int a=0;a<n_ages;a++){
     Z_init(a) =  F_minit(a) + M_at_Age(a);
   }
 
-  // initialize unfished population
-  for (int t=0; t<36; t++) { // run-out for 3 years to get rid of initial conditions
-    int m_ind = t % 12; // month index
-    for(int a=0;a<n_ages;a++){
-      if (a==0) {
-        if (t==0) {
-          N_unfished(a,m_ind) = R0_m(m_ind);
-        } else {
-          N_unfished(a,m_ind) = BH_SRR(R0_m(m_ind), h, SB_init(m_ind), SBpR);
-        }
-      } else {
-        if (m_ind==0) {
-          N_unfished(a,m_ind) = N_unfished(a-1,11) * exp(-Z_init(a-1)) * (1-PSM_at_Age(a-1));
-        } else {
-          N_unfished(a,m_ind) = N_unfished(a-1,m_ind-1) * exp(-Z_init(a-1)) * (1-PSM_at_Age(a-1));
-        }
-      }
-      SB_ainit(a,m_ind) = N_unfished(a,m_ind) * Weight_Age(a) * Mat_at_Age(a) * exp(-F_minit(a)/2);
-    }
-    SB_init(m_ind) = SB_ainit.col(m_ind).sum();
-  }
-
-  // first fished age-classes - month = 0
-  // F, M, and Z by month and age
-  // vector<Type> Z_init(n_ages);
-  // Z_init.setZero();
-  //
-  // for(int a=0;a<n_ages;a++){
-  //   Z_init(a) =  F_minit(a) + M_at_Age(a);
-  // }
-  matrix<Type> N_m(n_ages, n_months);
+  matrix<Type> N_m(n_ages, n_months); // N by age and month
   N_m.setZero();
-  //
-  matrix<Type> SB_am(n_ages, n_months);
-  SB_am.setZero();
 
-  vector<Type> SB_m(n_months);
-  SB_m.setZero();
-  //
-  // for(int a=1;a<n_ages;a++){
-  //   N_m(a,0) = N_unfished(a-1,11) * exp(-Z_init(a-1)) * (1-PSM_at_Age(a-1));
-  //   SB_am(a,0) = N_m(a,0) * Weight_Age(a) * Mat_at_Age(a) * exp(-F_minit(a)/2);
-  // }
-  //
-  // SB_m(0) = SB_am.col(0).sum();
-  //
-  // N_m(0,0) = BH_SRR(R0_m(0), h, SB_m(0), SBpR) * exp(logRec_Devs(0) - pow(sigmaR,2)/Type(2.0));
-
-  for(int a=0;a<n_ages;a++){
-    N_m(a,0) = N_unfished(a,11);
+  for(int a=1;a<n_ages;a++){
+    N_m(a,0) = N_unfished(a-1,11) * exp(-Z_init(a-1)) * (1-PSM_at_Age(a-1));
+    SB_am(a,0) =  N_m(a,0) * Weight_Age(a) * Mat_at_Age(a) * exp(-F_minit(a)/2);
   }
+  SB_m(0) = SB_am.col(0).sum(); // spawning biomass in initial month
+  // recruitment in initial month
+  N_m(0,0) = BH_SRR(R0_m(0), h, SB_m(0), SBpR) * exp(logRec_Devs(0) - pow(sigmaR,2)/Type(2.0));
 
-  // loop over remaining months
+  // ---- Population dynamics for remaining months ----
   for (int m=1; m<n_months; m++) {
     int m_ind = m % 12; // calendar month index
     for(int a=1;a<n_ages;a++){
@@ -258,13 +187,14 @@ Type SLAM(objective_function<Type>* obj) {
     N_m(0,m) = BH_SRR(R0_m(m_ind), h, SB_m(m), SBpR) * exp(logRec_Devs(m) - pow(sigmaR,2)/Type(2.0));
   }
 
-  // Calculate catch
-  matrix<Type> predC_a(n_ages, n_months);
-  matrix<Type> predCB_a(n_ages, n_months);
-  vector<Type> predCB(n_months);
+  // ---- Calculate catch ----
+  matrix<Type> predC_a(n_ages, n_months); // catch numbers by age and month
+  matrix<Type> predCB_a(n_ages, n_months); // catch biomass by age and month
+  vector<Type> predCB(n_months); // catch biomass by month
   predC_a.setZero();
   predCB_a.setZero();
   predCB.setZero();
+
   for (int m=0; m<n_months; m++) {
     for(int a=0;a<n_ages;a++){
       predC_a(a,m) = N_m(a,m)*((1-Mat_at_Age(a))*exp(-M_ma(a,m)/2)+Mat_at_Age(a)*exp(-PSM_at_Age(a)/2))*(1-exp(-F_ma(a,m)));
@@ -284,6 +214,7 @@ Type SLAM(objective_function<Type>* obj) {
     }
   }
 
+  // Standardize CAW data to sum to one
   for (int m=0; m<n_months; m++) {
     Type temp = predCAW.col(m).sum();
     for(int w=0;w<n_bins;w++){
@@ -291,9 +222,9 @@ Type SLAM(objective_function<Type>* obj) {
     }
   }
 
-  // Likelihoods
+  // ---- Calculate likelihoods ----
 
-  // Catch-at-Weight
+  // --- Catch-at-Weight ---
   vector<Type> CAWns(n_months);
   CAWns.setZero();
   for (int m=0; m<n_months; m++) {
@@ -304,12 +235,18 @@ Type SLAM(objective_function<Type>* obj) {
   CAWnll.setZero();
   for (int m=0; m<n_months; m++) {
     if (CAWns(m)>0) {
+
+      // standardize observed CAW to sum 1
       vector<Type> CAWp_obs(n_bins);
       CAWp_obs.setZero();
       CAWp_obs = CAW.col(m)/CAW.col(m).sum();
+
+      // scale by assumed sample size
       vector<Type> N_obs(n_bins);
       N_obs.setZero();
       N_obs = CAW_ESS(m) * CAWp_obs;
+
+      // multinomial likelihood
       vector<Type> pred(n_bins);
       pred.setZero();
       pred = predCAW.col(m);
@@ -450,6 +387,51 @@ Type SLAM(objective_function<Type>* obj) {
 
   Type nll=0;
   nll = nll_joint.sum();
+
+
+  // Calculate SPR
+  // Calculate spawning biomass per recruit
+  vector<Type> surv0(n_ages);
+  surv0.setZero();
+  surv0(0) = 1;
+  vector<Type> egg0(n_ages);
+  egg0.setZero();
+  for (int a=1; a<n_ages; a++) {
+    surv0(a) = surv0(a-1)*exp(-M_ma(a-1,0))*(1-PSM_at_Age(a-1));
+  }
+  for (int a=0; a<n_ages; a++) {
+    egg0(a) = surv0(a) * Weight_Age(a) * Mat_at_Age(a);
+  }
+  Type SBpR = egg0.sum();
+
+
+  matrix<Type> survF(n_ages, n_months);
+  survF.setZero();
+  vector<Type> eggFa(n_ages);
+  eggFa.setZero();
+  vector<Type> eggF(n_months);
+  eggF.setZero();
+
+  for (int m=0; m<n_months; m++) {
+    for(int a=0;a<n_ages;a++){
+      if (a==0) {
+        survF(a,m) = 1;
+      } else {
+        survF(a,m) = survF(a-1,m)*exp(-Z_ma(a-1, m)) * (1-PSM_at_Age(a-1));
+      }
+      eggFa(a) = survF(a,m) * Weight_Age(a) * Mat_at_Age(a);
+    }
+    eggF(m) = eggFa.sum();
+  }
+
+  vector<Type> SPR(n_months);
+  SPR.setZero();
+  for (int m=0; m<n_months; m++) {
+    SPR(m) = eggF(m)/SBpR;
+  }
+
+
+
 
   // Reports
   ADREPORT(SPR);
