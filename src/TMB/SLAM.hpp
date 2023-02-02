350d9f7356b1893744e5849ab7543987aea1240d
Adrian <ar.hordyk@gmail.com>
2023-02-02 14:37:40 -0800

    Update SLAM.hpp

--- src/TMB/SLAM.hpp
+++ src/TMB/SLAM.hpp
@@ -208,22 +208,13 @@ Type SLAM(objective_function<Type>* obj) {
   matrix<Type> N_m(n_ages, n_months); // N by age and month
   N_m.setZero();
 
-  for (int t=0; t<120; t++) {
-    int m_ind = t % 12; // calendar month index
-    for(int a=1;a<n_ages;a++){
-      if (t=0) {
-        N_m(a,0) = N_unfished(a-1,11) * exp(-Z_init(a-1)) * (1-PSM_at_Age(a-1));
-      } else {
-        N_m(a,0) = N_m(a-1,0) * exp(-Z_init(a-1)) * (1-PSM_at_Age(a-1));
-      }
-      SB_am(a,0) =  N_m(a,0) * Weight_Age(a) * Mat_at_Age(a) * exp(-F_minit(a)/2);
-    }
-    SB_m(0) = SB_am.col(0).sum(); // spawning biomass in initial month
-    N_m(0,0) = BH_SRR(R0_m(m_ind), h, SB_m(0), SBpR) ;
+  for(int a=1;a<n_ages;a++){
+    N_m(a,0) = N_unfished(a-1,11) * exp(-Z_init(a-1)) * (1-PSM_at_Age(a-1));
+    SB_am(a,0) =  N_m(a,0) * Weight_Age(a) * Mat_at_Age(a); //  * exp(-F_minit(a)/2);
   }
 
   // recruitment in initial month
-  // N_m(0,0) = BH_SRR(R0_m(0), h, SB_m(0), SBpR) * exp(logRec_Devs(0) - pow(sigmaR,2)/Type(2.0));
+  N_m(0,0) = BH_SRR(R0_m(0), h, SB_m(0), SBpR) * exp(logRec_Devs(0) - pow(sigmaR,2)/Type(2.0));
 
   // ---- Population dynamics for remaining months ----
   for (int m=1; m<n_months; m++) {
@@ -235,7 +226,6 @@ Type SLAM(objective_function<Type>* obj) {
     SB_m(m) = SB_am.col(m).sum();
     // recruitment
     N_m(0,m) = BH_SRR(R0_m(m_ind), h, SB_m(m), SBpR) * exp(logRec_Devs(m) - pow(sigmaR,2)/Type(2.0));
-
   }
 
   // ---- Calculate catch ----
