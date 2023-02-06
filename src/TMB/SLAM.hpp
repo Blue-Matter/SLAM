07fc4407b64fd01be6eedbce63b10a2071374430
Adrian <ar.hordyk@gmail.com>
2023-02-06 11:29:02 -0800

    Update SLAM.hpp

--- src/TMB/SLAM.hpp
+++ src/TMB/SLAM.hpp
@@ -125,11 +125,11 @@ Type SLAM(objective_function<Type>* obj) {
   }
   vector<Type> rec_dev_m(n_months);
   rec_dev_m.setZero();
-  int year = -1;
+  int year_ind = -1;
   for (int m=0; m<n_months; m++) {
     int m_ind = m % 12; // month index
-    if (m_ind==0) year = year +1;
-    rec_dev_m(m) = rec_dev_y(year);
+    if (m_ind==0) year_ind = year_ind +1;
+    rec_dev_m(m) = rec_dev_y(year_ind);
   }
 
 
