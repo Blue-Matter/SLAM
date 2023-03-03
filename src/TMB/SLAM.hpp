8285d670feef46f2c34495239e0bffa6fdcb9227
Adrian <ar.hordyk@gmail.com>
2023-03-03 14:27:42 -0800

    Update SLAM.hpp

--- src/TMB/SLAM.hpp
+++ src/TMB/SLAM.hpp
@@ -258,7 +258,7 @@ Type SLAM(objective_function<Type>* obj) {
   for(int a=1;a<n_ages;a++){
     N_m(a,0) = N_fished_eq(a-1,11) * exp(-Za_init(a-1)) * (1-PSM_at_Age(a-1));
     SB_am(a,0) =  N_m(a,0) * Weight_Age(a) * Mat_at_Age(a)  * exp(-Fa_init(a)/2);
-    B_am(a,0) = N_m(a,0) * Weight_Age(a) * exp(-F_ma(a,0)/2);
+    B_am(a,0) = N_m(a,0) * Weight_Age(a);
   }
 
   // recruitment in initial month
@@ -272,7 +272,7 @@ Type SLAM(objective_function<Type>* obj) {
     for(int a=1;a<n_ages;a++){
       N_m(a,m) = N_m(a-1,m-1) * exp(-Z_ma(a-1, m-1)) * (1-PSM_at_Age(a-1));
       SB_am(a,m) = N_m(a,m) * Weight_Age(a) * Mat_at_Age(a) * exp(-F_ma(a,m)/2);
-      B_am(a,m) = N_m(a,m) * Weight_Age(a) * exp(-F_ma(a,m)/2);
+      B_am(a,m) = N_m(a,m) * Weight_Age(a);
     }
     SB_m(m) = SB_am.col(m).sum();
     B_m(m) = B_am.col(m).sum();
