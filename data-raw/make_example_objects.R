8e0a4825bb86b129d1316a96a013762c1f2ea63c
Adrian <ar.hordyk@gmail.com>
2023-03-08 13:47:21 -0800

    update data

--- data-raw/make_example_objects.R
+++ data-raw/make_example_objects.R
@@ -1,8 +1,30 @@
 
-LifeHistory <- Import_LifeHistory()
-Exploitation <- Import_Exploitation()
-Sampling <- Import_Sampling()
+LifeHistory <- Import_LifeHistory(dev=TRUE)
+Exploitation <- Import_Exploitation(dev=TRUE)
+Sampling <- Import_Sampling(dev=TRUE)
+
+LifeHistory_No_Error <- LifeHistory
+LifeHistory_No_Error$sigmaR <- 0.0001
+LifeHistory_No_Error$R0_bar <- 1
+
+Exploitation_No_Error <- Exploitation
+Exploitation_No_Error$q_cv <- 0.0001
+
+
+Perfect_Sampling <- SLAM::Sampling
+Perfect_Sampling$CPUE_CV <- 0.0001
+Perfect_Sampling$Catch_CV <- 0.0001
+Perfect_Sampling$Effort_CV <- 0.0001
+Perfect_Sampling$CAW_Annual_ESS <- 1E6
+Perfect_Sampling$CAW_Annual_Sample_Size <- 1E6
+Perfect_Sampling$CAA_Annual_ESS <- 1E6
+Perfect_Sampling$CAA_Annual_Sample_Size <- 1E6
+
 
 usethis::use_data(LifeHistory, overwrite = TRUE)
 usethis::use_data(Exploitation, overwrite = TRUE)
 usethis::use_data(Sampling, overwrite = TRUE)
+
+usethis::use_data(LifeHistory_No_Error, overwrite = TRUE)
+usethis::use_data(Exploitation_No_Error, overwrite = TRUE)
+usethis::use_data(Perfect_Sampling, overwrite = TRUE)
