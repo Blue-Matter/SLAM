3a0fac601b24271b91b037a2e775ab590e075941
Adrian <ar.hordyk@gmail.com>
2023-03-21 12:07:44 -0700

    Update make_example_objects.R

--- data-raw/make_example_objects.R
+++ data-raw/make_example_objects.R
@@ -3,12 +3,21 @@ LifeHistory <- Import_LifeHistory(dev=TRUE)
 Exploitation <- Import_Exploitation(dev=TRUE)
 Sampling <- Import_Sampling(dev=TRUE)
 
+LifeHistory_Pulse <- LifeHistory
+LifeHistory_Pulse$R0_m <- Generate_Monthly_Recruitment(6.5, 2.5)
+
 LifeHistory_No_Error <- LifeHistory
 LifeHistory_No_Error$sigmaR <- 0.0001
 LifeHistory_No_Error$R0_bar <- 1
 
+LifeHistory_Pulse_No_Error <- LifeHistory_Pulse
+LifeHistory_Pulse_No_Error$sigmaR <- 0.0001
+LifeHistory_Pulse_No_Error$R0_bar <- 1
+
+
 Exploitation_No_Error <- Exploitation
 Exploitation_No_Error$q_cv <- 0.0001
+Exploitation_No_Error$Effort_cv <- 0.0001
 
 
 Perfect_Sampling <- SLAM::Sampling
@@ -22,6 +31,7 @@ Perfect_Sampling$CAA_Annual_Sample_Size <- 1E6
 
 
 usethis::use_data(LifeHistory, overwrite = TRUE)
+usethis::use_data(LifeHistory_Pulse, overwrite = TRUE)
 usethis::use_data(Exploitation, overwrite = TRUE)
 usethis::use_data(Sampling, overwrite = TRUE)
 
