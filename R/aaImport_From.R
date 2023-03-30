57f1d49130cbafa71e1e7edfb30a422492433d52
Adrian <ar.hordyk@gmail.com>
2023-03-30 14:43:39 -0700

    Update aaImport_From.R

--- R/aaImport_From.R
+++ R/aaImport_From.R
@@ -1,8 +1,9 @@
 
 #' @importFrom dplyr %>%  all_of arrange bind_rows filter group_by left_join mutate pull select summarize
 #' summarise
-#' @importFrom ggplot2 aes element_blank expand_limits facet_wrap geom_boxplot geom_line
-#' ggplot ggplotGrob geom_rect geom_ribbon geom_point guides guide_legend labs theme theme_bw theme_classic xlim ylim xlab ylab
+#' @importFrom ggplot2 aes element_blank element_text expand_limits facet_wrap facet_grid geom_boxplot geom_line
+#' ggplot ggplotGrob geom_bar geom_rect geom_ribbon geom_point guides guide_legend labs scale_x_date theme
+#' theme_bw theme_classic xlim ylim xlab ylab
 #' @importFrom graphics abline arrows axis axTicks barplot boxplot contour hist
 #' @importFrom stats dnorm
 NULL
