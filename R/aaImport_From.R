cef14644568dc7cfa3d9f50dc0854f05ea1617f4
Adrian <ar.hordyk@gmail.com>
2023-03-31 09:08:57 -0700

    Update aaImport_From.R

--- R/aaImport_From.R
+++ R/aaImport_From.R
@@ -7,3 +7,54 @@
 #' @importFrom graphics abline arrows axis axTicks barplot boxplot contour hist
 #' @importFrom stats dnorm
 NULL
+
+
+#' Class Constructor
+#'
+#' Put some details about it
+#'
+#' @param type an object
+#'
+#' @export
+Create <- function(type='Data') {
+  if (type=='Data') {
+    return(Make_Data)
+  }
+}
+
+
+Make_Data <- function() {
+  data <- list()
+  data$Metadata <- data.frame(Field=c('Fishery',
+                                      'Location',
+                                      'Species',
+                                      'Common_Name',
+                                      'Author',
+                                      'Date'),
+                                     Value=NA)
+  data$Ages <- numeric()
+  data$Weight_Age <- numeric()
+  data$Weight_Age_SD <- numeric()
+  data$Maturity_at_Age <- numeric()
+  data$M_at_Age <- numeric()
+  data$Post_Spawning_Mortality <- numeric()
+  data$CAW <- numeric()
+  data$CAW_ESS
+  data$Weight_Bins
+  data$Weight_Mids
+  data$TS <- data.frame(Year=NA,
+                        Month=NA,
+                        Effort=NA,
+                        Effort_SD=NA,
+                        Index=NA,
+                        Index_SD=NA)
+  data$use_Frwpen <- 1
+  data$use_R0rwpen <- 1
+  data$model <- 'SLAM'
+  class(data) <- 'Data'
+  data
+}
+
+Make_Parameters <- function() {
+
+}
