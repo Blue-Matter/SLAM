01af7265f892543746824ab4bddb37f1e0f59a94
Adrian <ar.hordyk@gmail.com>
2023-06-13 12:14:35 -0700

    Update SLAM.hpp

--- src/TMB/SLAM.hpp
+++ src/TMB/SLAM.hpp
@@ -224,7 +224,8 @@ Type SLAM(objective_function<Type>* obj) {
   Mean_monthly_F =  monthly_mean(F_m);
 
   // calculate average monthly pattern in F
-  Type meanVal = Mean_monthly_F.sum()/Mean_monthly_F.size();
+  Type sumVal = Mean_monthly_F.sum()
+  Type meanVal = sumVal/12;
 
   vector<Type> relMean_monthly_F = Mean_monthly_F/meanVal;
 
