02027c7213467303468f28c876b5a75007b75d6a
Adrian <ar.hordyk@gmail.com>
2023-06-14 16:49:31 -0700

    Update Sim_Testing.R

--- R/Sim_Testing.R
+++ R/Sim_Testing.R
@@ -8,7 +8,7 @@
 #' @return A list
 #' @export
 #'
-Sim_Test <- function(x, grid, Simulation, Sampling) {
+Sim_Test <- function(x, grid, Simulation, Sampling, useSeasonalInit=1) {
 
   # Generate Data
   Sampling$n_recent_months <- grid$n_months[x]
@@ -20,9 +20,11 @@ Sim_Test <- function(x, grid, Simulation, Sampling) {
   for (i in 1:nsim) {
     message(i, '/', nsim)
     Data <- Import(Sampled_Data, sim=i, Data_types = grid$data_types[x])
+    Data$useSeasonalInit <- useSeasonalInit
 
     Parameters <- Initialize_Parameters(Data)
     assess <- Assess(Data, Parameters)
+
     outlist[[i]] <- assess
   }
 
@@ -33,5 +35,100 @@ Sim_Test <- function(x, grid, Simulation, Sampling) {
   out$Simulation <- Simulation
   out$Sampling <- Sampling
   out
+}
+
+
+
+
+#' Title
+#'
+#' @param sim
+#' @param ll
+#'
+#' @return
+#' @export
+#'
+calc_F_RE <- function(sim, ll) {
+
+  assess <- ll$assess[[sim]]
+  if(class(assess$opt) =='try-error')
+    return(NULL)
+  n_months <- ll$n_months
+  OM <- ll$Simulation$Time_Series %>%
+    filter(Sim==sim, Month_ind %in% assess$Data$Month_ind)
+  Year <- sort(assess$Data$Year)
+  Month <- assess$Data$Month
+  Year_Month <- paste(Year, Month, sep="_")
+
+  df <- data.frame(Year=Year,
+                   Month=Month,
+                   Year_Month=Year_Month,
+                   OM=OM$F_mort,
+                   Estimate=assess$rep$F_m)
+
+  df <- df %>% tail(n_months)
+  data.frame(RE=(median(df$Estimate)-median(df$OM))/median(df$OM),
+             n_months=ll$n_months, Data_types=ll$Data_types, Var='F')
+}
+
+#' Title
+#'
+#' @param sim
+#' @param ll
+#'
+#' @return
+#' @export
+#'
+calc_SB_SB0_RE <- function(sim, ll) {
+  assess <- ll$assess[[sim]]
+  if(class(assess$opt) =='try-error')
+    return(NULL)
+
+  n_months <- ll$n_months
+  OM <- ll$Simulation$Time_Series %>%
+    filter(Sim==sim, Month_ind %in% assess$Data$Month_ind)
+  Year <- sort(assess$Data$Year)
+  Month <- assess$Data$Month
+  Year_Month <- paste(Year, Month, sep="_")
+
+  df <- data.frame(Year=Year,
+                   Month=Month,
+                   Year_Month=Year_Month,
+                   OM=OM$SB_fished/OM$SB_unfished_eq,
+                   Estimate=assess$rep$SB_m/assess$rep$SB0_m)
+
+  df <- df %>% tail(n_months)
+  data.frame(RE=(median(df$Estimate)-median(df$OM))/median(df$OM),
+             n_months=ll$n_months, Data_types=ll$Data_types, Var='SB/SB0')
+}
+
+#' Title
+#'
+#' @param sim
+#' @param ll
+#'
+#' @return
+#' @export
+#'
+calc_SPR_RE <- function(sim, ll) {
+  assess <- ll$assess[[sim]]
+  if(class(assess$opt) =='try-error')
+    return(NULL)
+  n_months <- ll$n_months
+  OM <- ll$Simulation$Time_Series %>%
+    filter(Sim==sim, Month_ind %in% assess$Data$Month_ind)
+  Year <- sort(assess$Data$Year)
+  Month <- assess$Data$Month
+  Year_Month <- paste(Year, Month, sep="_")
+
+  df <- data.frame(Year=Year,
+                   Month=Month,
+                   Year_Month=Year_Month,
+                   OM=OM$SPR,
+                   Estimate=assess$rep$SPR)
+
+  df <- df %>% tail(n_months)
+  data.frame(RE=(median(df$Estimate)-median(df$OM))/median(df$OM),
+             n_months=ll$n_months, Data_types=ll$Data_types, Var='SPR')
 }
 
