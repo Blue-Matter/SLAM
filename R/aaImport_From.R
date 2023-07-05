a852be872eea5ad5b61de2e15e139b8a4dc45b90
Adrian <ar.hordyk@gmail.com>
2023-07-05 11:48:21 -0700

    update

--- R/aaImport_From.R
+++ R/aaImport_From.R
@@ -2,7 +2,7 @@
 #' @importFrom dplyr %>%  all_of arrange bind_rows filter group_by left_join mutate pull select summarize
 #' summarise
 #' @importFrom ggplot2 aes element_blank element_text expand_limits facet_wrap facet_grid geom_boxplot geom_line
-#' ggplot ggplotGrob geom_bar geom_hline geom_rect geom_ribbon geom_point guides guide_legend labs scale_x_date theme
+#' ggplot ggplotGrob geom_bar geom_hline geom_rect geom_ribbon geom_text geom_point guides guide_legend labs scale_x_date theme
 #' theme_bw theme_classic xlim ylim xlab ylab
 #' @importFrom graphics abline arrows axis axTicks barplot boxplot contour hist
 #' @importFrom stats dnorm
